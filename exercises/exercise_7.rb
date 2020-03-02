73e0fb163cd6eb303514ba3c04661ef8ac2a6166
Lucy <lucymidgley13@gmail.com>
2020-03-02 20:56:34 +0000

    added validation for employee and store creation and ouptted the error messages to the console when adding a failing record

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# p = @store2.employees.create(first_name: "", last_name: "Bogie", hourly_rate: 10)
+# p = @store2.employees.create(first_name: "Nelly", last_name: "", hourly_rate: 10)
+# p = @store2.employees.create(first_name: "Nelly", last_name: "Bogie", hourly_rate: 1000)
+# p = @store2.employees.create(first_name: "Nelly", last_name: "Bogie", hourly_rate: 13.50)
+# p = @store2.employees.create(first_name: "Nelly", last_name: "Bogie", hourly_rate: 10)
+# p = Employee.create(first_name: "Nelly", last_name: "Bogie", hourly_rate: 10)
+# p = Store.create(name: "Bu", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+# p = Store.create(name: "Burnaby", annual_revenue: -1, mens_apparel: true, womens_apparel: true)
+# p = Store.create(name: "Burnaby", annual_revenue: 1.5, mens_apparel: true, womens_apparel: true)
+# p = Store.create(name: "Burnaby", annual_revenue: 10000, mens_apparel: false, womens_apparel: false)
+puts "Please enter a store name"
+store_new = gets.chomp
+p = Store.create(name: store_new)
+errors = p.errors.messages
+errors.each{|message|
+puts message}
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validate :must_carry_male_or_female_apparel
+  def must_carry_male_or_female_apparel
+  if !mens_apparel && !womens_apparel
+    errors.add(:mens_apparel, "Store must at least carry men's or women's apparel")
+    errors.add(:womens_apparel, "Store must at least carry men's or women's apparel")
+  end
+end
+
+
 end
